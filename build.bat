e6c723cd8fc38a441fe26bf0cc786233aa014374
Arthur Neufeld <aneufeld@seinesoftware.ca>
2020-02-20 12:29:38 -0600

    Include 2.4.2 in test

--- build.bat
+++ build.bat
@@ -8,5 +8,10 @@ py -3.7 -m pip install --user sphinx==2.0.0
 py -3.7 setup.py build_sphinx -b htmlhelp --build-dir build\sphinx-200
 %HHC% build\sphinx-200\htmlhelp\issue7188.hhp
 
+py -3.7 -m pip install --user sphinx==2.4.2
+py -3.7 setup.py build_sphinx -b htmlhelp --build-dir build\sphinx-242
+%HHC% build\sphinx-242\htmlhelp\issue7188.hhp
+
 start build\sphinx-185\htmlhelp\issue7188.chm
 start build\sphinx-200\htmlhelp\issue7188.chm
+start build\sphinx-242\htmlhelp\issue7188.chm
